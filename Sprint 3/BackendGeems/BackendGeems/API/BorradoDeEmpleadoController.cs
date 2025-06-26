441db543c70173d93ec1d93cd4c8ddb12b5b4c40
sebastian <sebastian.blancoquesada@uc.ac.cr>
2025-06-26 12:25:25 -0600

    Creacion del controlador de borrado de empleados

---
+++ Sprint 3/BackendGeems/BackendGeems/API/BorradoDeEmpleadoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using BackendGeems.Infraestructure;
+using BackendGeems.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendGeems.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorradoDeEmpleadoController : ControllerBase
+    {
+        private readonly GeneralRepo _generalRepo;
+        private readonly BorradoDeEmpleados borradoDeEmpleados;
+
+        public BorradoDeEmpleadoController(GeneralRepo generalRepo, BorradoDeEmpleados borradoDeEmpleados)
+        {
+            _generalRepo = generalRepo;
+            this.borradoDeEmpleados = borradoDeEmpleados;
+        }
+
+        [HttpPut]
+        public IActionResult BorrarEmpleado([FromQuery] string Cedula)
+        {
+            try
+            {
+                borradoDeEmpleados.BorrarEmpleado(Cedula);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}
