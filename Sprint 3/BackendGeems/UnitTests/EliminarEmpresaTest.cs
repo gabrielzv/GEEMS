4e1714e0b6c3f65875aef8cf3e3bc8aa0ed582d1
Mainorrr <mainor.cas.v@gmail.com>
2025-07-01 14:04:39 -0600

    Solucionando errores en unit tests

--- Sprint 3/BackendGeems/UnitTests/EliminarEmpresaTest.cs
+++ Sprint 3/BackendGeems/UnitTests/EliminarEmpresaTest.cs
@@ -4,6 +4,7 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UnitTests
 {
@@ -37,8 +38,25 @@ namespace UnitTests
         [Test]
         public void EliminarEmpresa_SinPagos_RealizaBorradoLogico()
         {
-            var cedula = "123";
-            _empresaRepoMock.Setup(r => r.GetEmpleados(cedula)).Returns(new List<Empleado>());
+            var cedula = "EMP123";
+            var empleados = new List<Empleado>
+            {
+                new Empleado { CedulaPersona = 1, Id = Guid.NewGuid() }
+            };
+
+            _empresaRepoMock.Setup(r => r.GetEmpleados(cedula)).Returns(empleados);
+            _pagoRepoMock.Setup(r => r.ContarPagos(It.IsAny<Guid>())).Returns(0);
+            _beneficioRepoMock.Setup(r => r.GetCompanyBenefits(cedula)).Returns(new List<object>());
+            _beneficioRepoMock.Setup(r => r.EliminarBeneficio(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.BorrarLogicoEmpleado(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.BorrarPermanenteEmpleado(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.UsuarioEstaBorrado(It.IsAny<string>())).Returns(false);
+            _empleadoRepoMock.Setup(r => r.ObtenerNombreEmpleadoPorCedula(It.IsAny<string>())).Returns("Empleado Test");
+            _empleadoRepoMock.Setup(r => r.ObtenerCorreoEmpleadoPorCedula(It.IsAny<string>())).Returns("empleado@test.com");
+            _empleadoRepoMock.Setup(r => r.ObtenerNombreEmpresaPorCedula(It.IsAny<string>())).Returns("Empresa Test");
+            _empleadoRepoMock.Setup(r => r.VerificarRelacionEmpleadoPlanilla(It.IsAny<string>())).Returns(false);
+            _empleadoRepoMock.Setup(r => r.BorrarTimesheetEmpleado(It.IsAny<string>(), It.IsAny<bool>()));
+            _reporteServiceMock.Setup(r => r.EnviarCorreoAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
             _empresaRepoMock.Setup(r => r.BorradoLogico(cedula));
 
             _queryEmpresa.EliminarEmpresa(cedula);
@@ -50,13 +68,25 @@ namespace UnitTests
         [Test]
         public void EliminarEmpresa_ConPagos_RealizaBorradoFisico()
         {
-            var cedula = "123";
+            var cedula = "EMP456";
             var empleados = new List<Empleado>
             {
-                new Empleado { Id = Guid.NewGuid(), CedulaPersona = 1 }
+                new Empleado { CedulaPersona = 2, Id = Guid.NewGuid() }
             };
+
             _empresaRepoMock.Setup(r => r.GetEmpleados(cedula)).Returns(empleados);
             _pagoRepoMock.Setup(r => r.ContarPagos(It.IsAny<Guid>())).Returns(1);
+            _beneficioRepoMock.Setup(r => r.GetCompanyBenefits(cedula)).Returns(new List<object>());
+            _beneficioRepoMock.Setup(r => r.EliminarBeneficio(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.BorrarLogicoEmpleado(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.BorrarPermanenteEmpleado(It.IsAny<string>()));
+            _empleadoRepoMock.Setup(r => r.UsuarioEstaBorrado(It.IsAny<string>())).Returns(false);
+            _empleadoRepoMock.Setup(r => r.ObtenerNombreEmpleadoPorCedula(It.IsAny<string>())).Returns("Empleado Test");
+            _empleadoRepoMock.Setup(r => r.ObtenerCorreoEmpleadoPorCedula(It.IsAny<string>())).Returns("empleado@test.com");
+            _empleadoRepoMock.Setup(r => r.ObtenerNombreEmpresaPorCedula(It.IsAny<string>())).Returns("Empresa Test");
+            _empleadoRepoMock.Setup(r => r.VerificarRelacionEmpleadoPlanilla(It.IsAny<string>())).Returns(true);
+            _empleadoRepoMock.Setup(r => r.BorrarTimesheetEmpleado(It.IsAny<string>(), It.IsAny<bool>()));
+            _reporteServiceMock.Setup(r => r.EnviarCorreoAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
             _empresaRepoMock.Setup(r => r.BorradoFisico(cedula));
 
             _queryEmpresa.EliminarEmpresa(cedula);
@@ -68,7 +98,7 @@ namespace UnitTests
         [Test]
         public void EliminarEmpresa_ThrowsException_PropagaExcepcion()
         {
-            var cedula = "123";
+            var cedula = "EMP789";
             _empresaRepoMock.Setup(r => r.GetEmpleados(cedula)).Throws(new Exception("Error"));
 
             Assert.Throws<Exception>(() => _queryEmpresa.EliminarEmpresa(cedula));
